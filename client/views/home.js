f7cfbb8b086cbd18e52f7ab8380eff579dde4287
weijian19391 <weijian19391@gmail.com>
2015-10-10 19:30:48 +0800

    added query for free time

--- client/views/home.js
+++ client/views/home.js
@@ -8,36 +8,26 @@ Template.home.helpers({
 	getCal: function(){
 		userId = Meteor.userId();
 		// console.log(userId);
-		 // var result = GoogleApi.get('calendar/v3/', {user: Meteor.users.findOne(userId)});
-		 // console.log(result);
-		GoogleApi.get('calendar/v3/users/me/calendarList', {user: Meteor.users.findOne(userId)}).then(function(result) {
+		// var result = GoogleApi.get('calendar/v3/', {user: Meteor.users.findOne(userId)});
+		// console.log(result);
+		jsonBody = {
+  		"timeMin": moment().format(),
+  		"timeMax": moment().add(7, 'days'),
+  		"groupExpansionMax": 1,
+  		"calendarExpansionMax": 1,
+  		"items": [
+    		{
+      		"id": "weijian19391@gmail.com"
+    		}
+  		]
+		};
+		GoogleApi.post('calendar/v3/freeBusy', {user: Meteor.users.findOne(userId),  data: jsonBody}).then(function(result) {
 							console.log(result);
+							console.log(moment().format());
 							console.log("inside callback");
 						}).fail(function(err) {
 					console.log(err);
 				});
-		// authencode = Meteor.users.findOne(userId).services.google.accessToken;
-		// var url = "https://www.googleapis.com/youtube/v3/search";
-		// console.log(authencode);
-		//   var options = {
-		//     'headers' : {
-		//       'Content-Type': 'application/json',
-		//       'Authorization': 'Bearer ' +  authencode,
-		//       'X-JavaScript-User-Agent': "Google APIs Explorer"
-		//     },
-		//     'params' : {
-		//        part : 'snippet',
-		//        q : 'cats',
-		//        maxResults : 25
-		//     }
-		//   };
-
-		//   var searchResult = HTTP.get(url, options).then(function(results) {
-		//   	console.log(result);
-	//       console.log("inside callback");
-	//     }).fail(function(err) {
-	//     	console.log(err);
-		//   });
 
 	}
 });
