486a367a39bb10ba772d46fbacda433d6c613bbe
weijian19391 <weijian19391@gmail.com>
2015-10-11 00:29:50 +0800

    added fb name to google doc

--- client/views/google.js
+++ client/views/google.js
@@ -1,12 +1,14 @@
 Template.google.events({
   'click #google-login': function(event) {
-  	fb_user = Meteor.userId();
+  	fbDoc_user = Meteor.userId();
+  	fb_name = Meteor.user().services.facebook.name;
     Meteor.loginWithGoogle({requestPermissions: ['https://www.googleapis.com/auth/calendar'], forceApprovalPrompt: true}, function(err){
       if (err) {
         throw new Meteor.Error("Google login failed");
       } else {
       	google_services = Meteor.users.findOne(Meteor.userId()).services;
-      	Meteor.users.update({_id:Meteor.userId()},{$set: {'profile.facebookId': fb_user}});
+      	Meteor.users.update({_id:Meteor.userId()},{$set: {'profile.facebookDocId': fbDoc_user}});
+        Meteor.users.update({_id:Meteor.userId()},{$set: {'profile.facebookName': fb_name}});
         Router.go('/');
       }
     });
