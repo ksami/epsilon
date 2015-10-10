9a5711b73b02e68dacfb9f427ffbc471a89b48d3
ksami <ksami.ihide@gmail.com>
2015-10-10 18:34:24 +0800

    use async meteor

--- server/fb.js
+++ server/fb.js
@@ -3,16 +3,25 @@ Meteor.publish(null, function(){
 });
 
 Meteor.methods({
-    getUserData: function(){
+    getUserFriends: function(callback){
         if(Meteor.userId && Meteor.user() && Meteor.user().services && Meteor.user().services.facebook){
             let fbId = Meteor.user().services.facebook.id;
             let token = Meteor.user().services.facebook.accessToken;
-            FBGraph.get(`/${fbId}/friends?access_token=${token}`, function(err, res) {
-                console.log(res);
-            });
+            let FBGraphSync = Meteor.wrapAsync(FBGraph.get);
+            let result = FBGraphSync(`/${fbId}/friends?access_token=${token}`);
+            return result;
         }
         else{
             throw new Meteor.Error("not-logged-in", "User not logged in");
+        }
+    },
+
+    getUserFacebookId: function(){
+        if(Meteor.userId && Meteor.user() && Meteor.user().services && Meteor.user().services.facebook){
+            return Meteor.user().services.facebook.id;
+        }
+        else{
+            return null;
         }
     }
 });
