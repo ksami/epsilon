d5ca2307ba8802926dc298f02374c84fa4057136
weijian19391 <weijian19391@gmail.com>
2015-10-11 04:38:56 +0800

    added lobby.js for lobby.html

--- client/views/main.js
+++ client/views/main.js
@@ -8,47 +8,48 @@ if(Meteor.isClient){
   });
 
   Template.main.helpers({
-
-	getCal: function(){
-		userId = Meteor.userId();
-		var tz = jstz.determine();
-		Meteor.call('getUserFriends', function(error, result){
-			// console.log(result);
-			for (var i = 0; i < result.data.length; i++) {
-				// console.log(result.data[i]);
-				currentFriend = Meteor.users.findOne({"profile.facebookName":result.data[i].name});
-				console.log(currentFriend);
-				currentFriendEmail = currentFriend.services.google.email;
-				jsonBody = {
-		  		"timeMin": moment().format(),
-		  		"timeMax": moment().add(7, 'days'),
-		  		"groupExpansionMax": 1,
-		  		"calendarExpansionMax": 1,
-		  		"timeZone" : tz.name(),
-		  		"items": [
-		    		{
-		      		"id": currentFriendEmail,
-		    		}
-		  		]
-				};
-				GoogleApi.post('calendar/v3/freeBusy', {user: currentFriend,  data: jsonBody}).then(function(result) {
-					console.log(result);
-					freeTime = [];
-					currenTime = moment().format();
-					for (var i = 0; i < result.calendars[currentFriendEmail].busy.length; i++) {
-						// console.log(result.calendars[currentFriendEmail].busy[i]);
-						if (currenTime != result.calendars[currentFriendEmail].busy[i].start)
-							freeTime.push({"startTime" : currenTime, "endTime" : result.calendars[currentFriendEmail].busy[i].start});
-						currenTime = result.calendars[currentFriendEmail].busy[i].end;
-					}
-					console.log(freeTime);
-					Session.set(currentFriendEmail+'freeTime', freeTime);
-				}).fail(function(err) {
-					console.log(err);
-				});
-			}
-		});
-	}
-});
-
+		getCal: function(){
+			userId = Meteor.userId();
+			var tz = jstz.determine();
+			Meteor.call('getUserFriends', function(error, result){
+				// console.log(result);
+				for (var i = 0; i < result.data.length; i++) {
+					// console.log(result.data[i]);
+					currentFriend = Meteor.users.findOne({"profile.facebookName":result.data[i].name});
+					console.log(currentFriend);
+					currentFriendEmail = currentFriend.services.google.email;
+					jsonBody = {
+			  		"timeMin": moment().format(),
+			  		"timeMax": moment().add(7, 'days'),
+			  		"groupExpansionMax": 1,
+			  		"calendarExpansionMax": 1,
+			  		"timeZone" : tz.name(),
+			  		"items": [
+			    		{
+			      		"id": currentFriendEmail,
+			    		}
+			  		]
+					};
+					GoogleApi.post('calendar/v3/freeBusy', {user: currentFriend,  data: jsonBody}).then(function(result) {
+						console.log(result);
+						freeTime = [];
+						currenTime = moment().format();
+						for (var i = 0; i < result.calendars[currentFriendEmail].busy.length; i++) {
+							// console.log(result.calendars[currentFriendEmail].busy[i]);
+							if (currenTime != result.calendars[currentFriendEmail].busy[i].start)
+								freeTime.push({"startTime" : currenTime, "endTime" : result.calendars[currentFriendEmail].busy[i].start});
+							currenTime = result.calendars[currentFriendEmail].busy[i].end;
+						}
+						console.log(freeTime);
+						Session.set(currentFriendEmail+'freeTime', freeTime);
+					}).fail(function(err) {
+						console.log(err);
+					});
+				}
+			});
+		},
+		sendNotif: function(){
+			Notifications.addNotification("this is a notification", "my notification is saying BS", {type:3, userCloseable: true});
+		}
+	});
 }
